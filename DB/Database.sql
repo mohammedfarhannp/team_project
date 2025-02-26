dfda9665fe27df983d264fc40121892c60e35437
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-02-26 20:47:04 +0530

    Database Updated with MCA Student Info
    More Data to update

--- DB/Database.sql
+++ DB/Database.sql
@@ -36,7 +36,43 @@ CREATE TABLE `AUTH` (
 LOCK TABLES `AUTH` WRITE;
 /*!40000 ALTER TABLE `AUTH` DISABLE KEYS */;
 INSERT INTO `AUTH` VALUES
-('IT24GMCAD11','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C');
+('IT24GMCAD01','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD02','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD03','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD04','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD05','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD06','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD07','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD08','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD09','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD10','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD11','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD12','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD13','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD14','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD15','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD16','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD17','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD18','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD19','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD20','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD21','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD22','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD23','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD24','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD25','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD26','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD27','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD28','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD29','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD30','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD31','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD32','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD33','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD34','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD35','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD36','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C'),
+('IT24GMCAD37','$2y$10$5bUBc1jlcEi9Bczliyo9quq4O7vfIsGVpug7EzteGobZc8ybPb.4C');
 /*!40000 ALTER TABLE `AUTH` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +132,43 @@ CREATE TABLE `RESULT_MASTERS` (
 LOCK TABLES `RESULT_MASTERS` WRITE;
 /*!40000 ALTER TABLE `RESULT_MASTERS` DISABLE KEYS */;
 INSERT INTO `RESULT_MASTERS` VALUES
-('IT24GMCAD11',8.24,NULL,NULL,NULL);
+('IT24GMCAD01',7.33,NULL,NULL,NULL),
+('IT24GMCAD02',7.90,NULL,NULL,NULL),
+('IT24GMCAD03',8.38,NULL,NULL,NULL),
+('IT24GMCAD04',6.14,NULL,NULL,NULL),
+('IT24GMCAD05',7.62,NULL,NULL,NULL),
+('IT24GMCAD06',8.29,NULL,NULL,NULL),
+('IT24GMCAD07',8.52,NULL,NULL,NULL),
+('IT24GMCAD08',7.81,NULL,NULL,NULL),
+('IT24GMCAD09',7.38,NULL,NULL,NULL),
+('IT24GMCAD10',7.19,NULL,NULL,NULL),
+('IT24GMCAD11',8.24,NULL,NULL,NULL),
+('IT24GMCAD12',7.67,NULL,NULL,NULL),
+('IT24GMCAD13',7.90,NULL,NULL,NULL),
+('IT24GMCAD14',7.19,NULL,NULL,NULL),
+('IT24GMCAD15',7.29,NULL,NULL,NULL),
+('IT24GMCAD16',6.00,NULL,NULL,NULL),
+('IT24GMCAD17',7.24,NULL,NULL,NULL),
+('IT24GMCAD18',7.00,NULL,NULL,NULL),
+('IT24GMCAD19',7.81,NULL,NULL,NULL),
+('IT24GMCAD20',8.52,NULL,NULL,NULL),
+('IT24GMCAD21',8.29,NULL,NULL,NULL),
+('IT24GMCAD22',8.00,NULL,NULL,NULL),
+('IT24GMCAD23',7.86,NULL,NULL,NULL),
+('IT24GMCAD24',7.81,NULL,NULL,NULL),
+('IT24GMCAD25',7.43,NULL,NULL,NULL),
+('IT24GMCAD26',7.48,NULL,NULL,NULL),
+('IT24GMCAD27',7.71,NULL,NULL,NULL),
+('IT24GMCAD28',7.38,NULL,NULL,NULL),
+('IT24GMCAD29',7.24,NULL,NULL,NULL),
+('IT24GMCAD30',6.81,NULL,NULL,NULL),
+('IT24GMCAD31',8.62,NULL,NULL,NULL),
+('IT24GMCAD32',7.48,NULL,NULL,NULL),
+('IT24GMCAD33',7.33,NULL,NULL,NULL),
+('IT24GMCAD34',8.24,NULL,NULL,NULL),
+('IT24GMCAD35',8.33,NULL,NULL,NULL),
+('IT24GMCAD36',7.62,NULL,NULL,NULL),
+('IT24GMCAD37',7.90,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `RESULT_MASTERS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +196,43 @@ CREATE TABLE `STUDENTS` (
 LOCK TABLES `STUDENTS` WRITE;
 /*!40000 ALTER TABLE `STUDENTS` DISABLE KEYS */;
 INSERT INTO `STUDENTS` VALUES
-('IT24GMCAD24','Mohammed Farhan N P','m.farhan.np.mca@gmail.com','MCA','2024-2026');
+('IT24GMCAD01','Abhinesh K Ramesh','abhineshkramesh007@gmail.com','MCA','2024-2026'),
+('IT24GMCAD02','Adith V','adithv137@gmail.com','MCA','2024-2026'),
+('IT24GMCAD03','Akash Gopalan','akashpryrm@gmail.com','MCA','2024-2026'),
+('IT24GMCAD04','Akash Pradeep','akash27m2@gmail.com','MCA','2024-2026'),
+('IT24GMCAD05','Akshay Saji','akshaysaji1312@gmail.com','MCA','2024-2026'),
+('IT24GMCAD06','Anugrah.M.V','anugrahmv007@gmail.com','MCA','2024-2026'),
+('IT24GMCAD07','Anuraj. A','karadkanuraj@gmail.com','MCA','2024-2026'),
+('IT24GMCAD08','Arjun P V','arjunsugu2002@gmail.com','MCA','2024-2026'),
+('IT24GMCAD09','Karthik K V','karthikmarar.11@gmail.com','MCA','2024-2026'),
+('IT24GMCAD10','Krishnendh M','krishnendhkrish7@gmail.com','MCA','2024-2026'),
+('IT24GMCAD11','Mohammed Farhan N P','m.farhan.np.mca@gmail.com','MCA','2024-2026'),
+('IT24GMCAD12','Muhammed Mishab P','mishab1441@gmail.com','MCA','2024-2026'),
+('IT24GMCAD13','Rohith. K','rohithkmca@gmail.com','MCA','2024-2026'),
+('IT24GMCAD14','Sachin K','sachinsajeev3121@gmail.com','MCA','2024-2026'),
+('IT24GMCAD15','Sai Sangeeth','saisangeeth50@gmail.com','MCA','2024-2026'),
+('IT24GMCAD16','Sanjay Kumar C','skthamilan1322003@gmail.com','MCA','2024-2026'),
+('IT24GMCAD17','Sayooj Valsan','sayoojvalsan2317@gmail.com','MCA','2024-2026'),
+('IT24GMCAD18','Sooraj K','soorajsoorajk136@gmail.com','MCA','2024-2026'),
+('IT24GMCAD19','S P Lukumanul Hakeem',NULL,'MCA','2024-2026'),
+('IT24GMCAD20','Abhishna P P','abhishna2002@gmail.com','MCA','2024-2026'),
+('IT24GMCAD21','Amrutha Balan','amruthabala18@gmail.com','MCA','2024-2026'),
+('IT24GMCAD22','Archana T K','archanatk27@gmail.com','MCA','2024-2026'),
+('IT24GMCAD23','Ardra K','ardmmz234@gmail.com','MCA','2024-2026'),
+('IT24GMCAD24','Fahma Rudhsha Latheef .K.T','fahrudlat@gmail.com','MCA','2024-2026'),
+('IT24GMCAD25','Fathima.N.K','fathimarahim678@gmail.com','MCA','2024-2026'),
+('IT24GMCAD26','Fathimath Jumana','fathimathjumana347@gmail.com','MCA','2024-2026'),
+('IT24GMCAD27','Fathimathu Ziya V K','ziafathima175@gmail.com','MCA','2024-2026'),
+('IT24GMCAD28','Keerthana P K','keerthanaharidas03@gmail.com','MCA','2024-2026'),
+('IT24GMCAD29','Maya . S','ammumaya0646@gmail.com','MCA','2024-2026'),
+('IT24GMCAD30','Mubashira.V.P','mubashiravpktr@gmail.com','MCA','2024-2026'),
+('IT24GMCAD31','Nada Fathima','nadaevk@gmail.com','MCA','2024-2026'),
+('IT24GMCAD32','Nandana.P.P','nandanamanoharan2002@gmail.com','MCA','2024-2026'),
+('IT24GMCAD33','Navya.P.P',NULL,'MCA','2024-2026'),
+('IT24GMCAD34','Neha Latheesh','ambilineha333@gmail.com','MCA','2024-2026'),
+('IT24GMCAD35','Theertha V','theerthav2003@gmail.com','MCA','2024-2026'),
+('IT24GMCAD36','Theja K','thejat422@gmail.com','MCA','2024-2026'),
+('IT24GMCAD37','Vandana T T K','vandanavandu270@gmail.com','MCA','2024-2026');
 /*!40000 ALTER TABLE `STUDENTS` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -137,4 +245,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-25 17:23:24
+-- Dump completed on 2025-02-26 20:43:51
