d4cdc9009fa0643b47f6034511cea22b6ea878ba
Mohammed Farhan N P <m.farhan.np@gmail.com>
2025-03-09 22:38:03 +0530

    Updates!

--- IT_Dept_Main/faculty-dashboard.php
+++ IT_Dept_Main/faculty-dashboard.php
@@ -26,25 +26,58 @@
 ?>
 
     <div class="toggle-btn">
-        <form>
+        <form method="POST">
             <?php
+                $Button_IDs = array();
+                $Counter = 0;
                 while($row=$States->fetch_assoc())
                 {
                     $Name = $row["BUTTON_NAME"];
+                    $Course = $row["BUTTON_COURSE"];
+                    $ID = $row["BUTTON_ID"];
+                    $Batch = $row["BUTTON_BATCH"];
+
+                    $Button_IDs[$Counter] = $ID;
+                    $Counter++; 
+
                     $State = ($row["BUTTON_STATE"] === "ON") ? "checked" : "";
                     echo "
                     <div class='toggle-container'>
-                        <label>$Name</label>
+                        <label>$Batch $Course $Name</label>
                         <label class='switch'>
-                            <input type='checkbox' value='on' $State>
+                            <input type='checkbox' name='$ID' value='on' $State>
                             <span class='toggle'></span>
                         </label>
                     </div>
                     ";
                 }
             ?>
+
+            <button type="submit">Update</button>
         </form>
     </div>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] === "POST")
+    {
+        foreach($Button_IDs as $ID)
+        {
+            if(isset($_POST["$ID"]))
+            {
+                $State = "ON";
+            } else {
+                $State = "OFF";
+            }
+
+            $SQL_QUERY = "UPDATE Button_State SET BUTTON_STATE='$State' WHERE BUTTON_ID='$ID'";
+            $conn->query($SQL_QUERY);
+        }
+        $conn->close();
+        header("Location: " . $_SERVER["PHP_SELF"]);
+        exit();
+    }
+    
+?>
 
     </body>
 </html>
