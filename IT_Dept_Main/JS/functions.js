7d5b72c14e6bde70f81a19ffb5ec21994994c20b
Mohammed Farhan N P <m.farhan.np@gmail.com>
2025-03-14 22:49:54 +0530

    Updates!

--- IT_Dept_Main/JS/functions.js
+++ IT_Dept_Main/JS/functions.js
@@ -9,3 +9,15 @@ function alert_user(ID) {
     Warning.style.display = 'block';
     setTimeout(() => {Warning.style.display = 'none';}, 5000);
 }
+
+// For button color change when clicked
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".fac-btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            buttons.forEach(btn => btn.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
---
+++ IT_Dept_Main/faculty.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>
+            Faculty Dashboard
+        </title>
+
+        <link rel="stylesheet" href="CSS/button.css">
+        <link rel="stylesheet" href="CSS/base.css">
+
+        <script src="JS/functions.js"></script>
+    </head>
+
+    <body>
+        <div class="side-panel">
+            <button class="fac-btn">MCA</button>
+            <button class="fac-btn">MSc</button>
+            <button class="fac-btn">FYIMP</button>
+        </div>
+    </body>
+
+    
+</html>
